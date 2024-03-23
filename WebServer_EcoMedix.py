72ef4c1a2de71c8e27aad10ebcd402b104064296
Oveenux <ajrondon@uninorte.edu.co>
2024-03-23 16:59:05 -0500

    test 12

---
+++ WebServer_EcoMedix.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from host_AD import hostConfig
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Esta función renderiza la plantilla HTML que mostrará la gráfica
+    return render_template('InicioWP.html')
+
+if __name__ == '__main__':
+    app.run(**hostConfig)
