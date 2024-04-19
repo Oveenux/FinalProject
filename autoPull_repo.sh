9651b0beee3fb1eb8d2d647570a160aaf3cef0c8
Oveenux <ajrondon@uninorte.edu.co>
2024-04-19 09:22:13 -0500

    Test 20

--- autoPull_repo.sh
+++ autoPull_repo.sh
@@ -7,8 +7,11 @@ repetir_caracter() {
 
 contador=1
 while true; do
-    git pull origin main
-    echo -e "\e[32mPull $contador: $(TZ='America/Bogota' date +"%T")\e[0m"
+    if git pull origin main; then
+        echo -e "\e[32mPull $contador: Exitoso - - [$(TZ='America/Bogota' date +"%F %T")]\e[0m"
+    else
+        echo -e "\e[31mPull $contador: Fallido - - [$(TZ='America/Bogota' date +"%F %T")]\e[0m"
+    fi
     ((contador++))
     repetir_caracter '='
     # Espera 5 minutos antes de la próxima ejecución
