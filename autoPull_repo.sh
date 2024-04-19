3345bd678cf7ce1938efc8233f2aaf4d2345fdc1
Oveenux <ajrondon@uninorte.edu.co>
2024-04-18 22:00:22 -0500

    Update 22

--- autoPull_repo.sh
+++ autoPull_repo.sh
@@ -1,8 +1,16 @@
+repetir_caracter() {
+    local caracter="$1"
+    local ancho=$(tput cols)
+    local cadena=$(printf "%-${ancho}s" "$caracter")
+    echo "${cadena// /$caracter}"
+}
+
 contador=1
 while true; do
     git pull origin main
     echo -e "\e[32mPull $contador: $(TZ='America/Bogota' date +"%T")\e[0m"
     ((contador++))
+    repetir_caracter '='
     # Espera 5 minutos antes de la próxima ejecución
-    sleep 300
+    sleep 3
 done
