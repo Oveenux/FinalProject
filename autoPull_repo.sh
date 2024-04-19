9d4fcc5a5aaf02eaccc9987637ec39cfa25a9f04
Oveenux <ajrondon@uninorte.edu.co>
2024-04-18 21:45:45 -0500

    Update 20: autoPull file solution

--- autoPull_repo.sh
+++ autoPull_repo.sh
@@ -2,6 +2,7 @@ contador=1
 while true; do
     git pull origin main
     echo -e "\e[32mPull $contador: $(TZ='America/Bogota' date +"%T")\e[0m"
+    ((counter++))
     # Espera 5 minutos antes de la próxima ejecución
     sleep 300
 done
